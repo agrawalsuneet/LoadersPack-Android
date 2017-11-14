073c8bee76c6a23687a640479a31a1e26917011d
Suneet Agrawal <agrawalsuneet@gmail.com>
2017-11-14 20:20:00 +0000

    added values

--- app/src/main/java/com/agrawalsuneet/loaders/MainActivity.kt
+++ app/src/main/java/com/agrawalsuneet/loaders/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        containerLayout = findViewById(R.id.container)
+        //containerLayout = findViewById(R.id.container)
 
         //initClockLoader();
         initRippleLoader()
--- loaderspack/src/main/java/com/agrawalsuneet/loaderspack/loaders/RippleLoader.kt
+++ loaderspack/src/main/java/com/agrawalsuneet/loaderspack/loaders/RippleLoader.kt
@@ -3,7 +3,9 @@ package com.agrawalsuneet.loaderspack.loaders
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.support.v4.view.ViewCompat
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import com.agrawalsuneet.loaderspack.R
 import com.agrawalsuneet.loaderspack.basicviews.LoaderContract
@@ -13,36 +15,40 @@ import com.agrawalsuneet.loaderspack.basicviews.LoaderContract
  */
 class RippleLoader : View, LoaderContract {
 
-    var fromRadius: Float = 20.0f
+    var fromRadius: Float = 2.0f
 
-    var toRadius: Float = 160.0f
+    var toRadius: Float = 120.0f
         set(value) {
             field = value
             invalidate()
         }
 
-    var fromStroke: Float = 4.0f
+    var fromStroke: Float = 10.0f
 
-    var toStroke: Float = 16.0f
+    var toStroke: Float = 200.0f
         set(value) {
             field = value
             invalidate()
         }
-    var fromAlpha: Int = 255
+    var fromAlpha: Int = 150
         set(value) {
             field = if (value >= 255) 255 else value
         }
-    var toAlpha: Int = 40
+    var toAlpha: Int = 10
         set(value) {
             field = if (value >= 255) 255 else value
         }
+
+    var animSpeedMultiplier: Float = 1.0f
+
+    var waveDuration: Int = 100
 
     var waveColor: Int = resources.getColor(R.color.blue)
 
     private lateinit var wavePaint: Paint
     private var currentRadius: Float = 0f
     private var currentStroke: Float = 0f
-    private var currentAlpha: Int = 0
+    private var currentAlpha: Float = 0f
 
     private var centerPoint: Float = 0.0f
 
@@ -68,14 +74,6 @@ class RippleLoader : View, LoaderContract {
 
     }
 
-    private fun initValues() {
-        currentRadius = fromRadius
-        currentStroke = fromStroke
-        currentAlpha = fromAlpha
-
-        centerPoint = toRadius + toStroke
-    }
-
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
@@ -89,7 +87,15 @@ class RippleLoader : View, LoaderContract {
         wavePaint.style = Paint.Style.STROKE
         wavePaint.isAntiAlias = true
         wavePaint.strokeWidth = fromStroke
-        wavePaint.alpha = currentAlpha
+        wavePaint.alpha = fromAlpha
+    }
+
+    private fun initValues() {
+        currentRadius = fromRadius
+        currentStroke = fromStroke
+        currentAlpha = fromAlpha.toFloat()
+
+        centerPoint = toRadius + toStroke
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -97,6 +103,24 @@ class RippleLoader : View, LoaderContract {
 
         canvas.drawCircle(centerPoint, centerPoint, currentRadius, wavePaint)
 
-        //ViewCompat.postInvalidateOnAnimation(this)
+
+        currentRadius += ((toRadius - fromRadius) / waveDuration)
+        currentStroke += ((toStroke - fromStroke) / waveDuration)
+        currentAlpha -= ((fromAlpha.toFloat() - toAlpha.toFloat()) / waveDuration.toFloat())
+
+        Log.d("Current Radius", currentRadius.toString())
+        Log.d("Current Stroke", currentStroke.toString())
+
+        if (currentRadius >= toRadius) {
+            currentRadius = fromRadius
+            currentStroke = fromStroke
+            currentAlpha = fromAlpha.toFloat()
+        }
+
+
+        wavePaint.strokeWidth = currentStroke
+        wavePaint.alpha = currentAlpha.toInt()
+
+        ViewCompat.postInvalidateOnAnimation(this)
     }
 }
