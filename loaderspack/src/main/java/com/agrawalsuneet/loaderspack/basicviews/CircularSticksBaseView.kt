0ab64b6abd70d0e51c99f838ddb258af4bd4ccf8
Suneet Agrawal <agrawalsuneet@gmail.com>
2018-01-01 19:25:22 +0000

    added loader

---
+++ loaderspack/src/main/java/com/agrawalsuneet/loaderspack/basicviews/CircularSticksBaseView.kt
@@ -0,0 +1,107 @@
+package com.agrawalsuneet.loaderspack.basicviews
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+import com.agrawalsuneet.loaderspack.R
+
+/**
+ * Created by suneet on 12/31/17.
+ */
+
+open class CircularSticksBaseView : View, LoaderContract {
+
+    var noOfSticks: Int = 80
+
+    var outerCircleRadius: Float = 200.0f
+    var innerCircleRadius: Float = 100.0f
+
+    var sticksColor: Int = resources.getColor(R.color.grey)
+    var viewBackgroundColor: Int = resources.getColor(android.R.color.white)
+
+    private lateinit var sticksPaint: Paint
+    private lateinit var innerCirclePaint: Paint
+
+    private lateinit var outerCircleOval: RectF
+
+    constructor(context: Context) : super(context) {
+        initPaints()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        initAttributes(attrs)
+        initPaints()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        initAttributes(attrs)
+        initPaints()
+    }
+
+    override fun initAttributes(attrs: AttributeSet) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CircularSticksBaseView, 0, 0)
+
+        this.noOfSticks = typedArray
+                .getInteger(R.styleable.CircularSticksBaseView_stickbase_noOfSticks, 80)
+
+        this.outerCircleRadius = typedArray
+                .getDimension(R.styleable.CircularSticksBaseView_stickbase_outerCircleRadius, 200.0f)
+        this.innerCircleRadius = typedArray
+                .getDimension(R.styleable.CircularSticksBaseView_stickbase_innerCircleRadius, 100.0f)
+
+
+        this.sticksColor = typedArray
+                .getColor(R.styleable.CircularSticksBaseView_stickbase_stickColor, resources.getColor(R.color.grey))
+        this.viewBackgroundColor = typedArray
+                .getColor(R.styleable.CircularSticksBaseView_stickbase_viewBackgroundColor, resources.getColor(android.R.color.white))
+
+        typedArray.recycle()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(2 * outerCircleRadius.toInt(), 2 * outerCircleRadius.toInt())
+    }
+
+    private fun initPaints() {
+        sticksPaint = Paint()
+        sticksPaint.color = sticksColor
+        sticksPaint.style = Paint.Style.FILL
+        sticksPaint.isAntiAlias = true
+
+        outerCircleOval = RectF().apply {
+            left = 0f
+            top = 0f
+            right = 2 * outerCircleRadius
+            bottom = 2 * outerCircleRadius
+        }
+
+        innerCirclePaint = Paint()
+        innerCirclePaint.color = viewBackgroundColor
+        innerCirclePaint.style = Paint.Style.FILL
+        innerCirclePaint.isAntiAlias = true
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        if (noOfSticks < 1) {
+            noOfSticks = 8
+        }
+
+        val sweepAngle: Float = (360f / (2 * noOfSticks))
+        var startAngle: Float = (0 - (sweepAngle / 2))
+
+        for (i in 0 until noOfSticks) {
+            canvas.drawArc(outerCircleOval, startAngle, sweepAngle, true, sticksPaint)
+            startAngle += (2 * sweepAngle)
+        }
+
+        canvas.drawCircle(outerCircleRadius, outerCircleRadius, innerCircleRadius, innerCirclePaint)
+    }
+
+
+}
---
+++ loaderspack/src/main/java/com/agrawalsuneet/loaderspack/loaders/CircularRotatingSticksLoader.kt
@@ -0,0 +1,120 @@
+package com.agrawalsuneet.loaderspack.loaders
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewTreeObserver
+import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
+import android.view.animation.RotateAnimation
+import android.widget.LinearLayout
+import com.agrawalsuneet.loaderspack.R
+import com.agrawalsuneet.loaderspack.basicviews.CircularSticksBaseView
+import com.agrawalsuneet.loaderspack.basicviews.LoaderContract
+
+/**
+ * Created by suneet on 1/1/18.
+ */
+class CircularRotatingSticksLoader : LinearLayout, LoaderContract {
+
+    var noOfSticks: Int = 50
+
+    var outerCircleRadius: Float = 200.0f
+    var innerCircleRadius: Float = 100.0f
+
+    var sticksColor: Int = resources.getColor(R.color.grey)
+    var viewBackgroundColor: Int = resources.getColor(android.R.color.white)
+
+    var animDuration: Int = 5000
+
+    private lateinit var circularSticksBaseView: CircularSticksBaseView
+
+    constructor(context: Context) : super(context) {
+        initView()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        initAttributes(attrs)
+        initView()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        initAttributes(attrs)
+        initView()
+    }
+
+    override fun initAttributes(attrs: AttributeSet) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CircularRotatingSticksLoader, 0, 0)
+
+        this.noOfSticks = typedArray
+                .getInteger(R.styleable.CircularRotatingSticksLoader_rotatingstick_noOfSticks, 50)
+
+        this.outerCircleRadius = typedArray
+                .getDimension(R.styleable.CircularRotatingSticksLoader_rotatingstick_outerCircleRadius, 200.0f)
+        this.innerCircleRadius = typedArray
+                .getDimension(R.styleable.CircularRotatingSticksLoader_rotatingstick_innerCircleRadius, 100.0f)
+
+
+        this.sticksColor = typedArray
+                .getColor(R.styleable.CircularRotatingSticksLoader_rotatingstick_stickColor, resources.getColor(R.color.grey))
+        this.viewBackgroundColor = typedArray
+                .getColor(R.styleable.CircularRotatingSticksLoader_rotatingstick_viewBackgroundColor, resources.getColor(android.R.color.white))
+
+        this.animDuration = typedArray
+                .getInteger(R.styleable.CircularRotatingSticksLoader_rotatingstick_animDuration, 5000)
+
+        typedArray.recycle()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(2 * outerCircleRadius.toInt(), 2 * outerCircleRadius.toInt())
+    }
+
+    private fun initView() {
+        removeAllViews()
+        removeAllViewsInLayout()
+
+        val loaderView = this
+
+        circularSticksBaseView = CircularSticksBaseView(context)
+                .apply {
+                    noOfSticks = loaderView.noOfSticks
+                    outerCircleRadius = loaderView.outerCircleRadius
+                    innerCircleRadius = loaderView.innerCircleRadius
+
+                    sticksColor = loaderView.sticksColor
+                    viewBackgroundColor = loaderView.viewBackgroundColor
+                }
+
+        addView(circularSticksBaseView)
+
+        viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                startLoading()
+
+                val vto = loaderView.viewTreeObserver
+                vto.removeOnGlobalLayoutListener(this)
+            }
+        })
+    }
+
+    private fun startLoading() {
+
+        val rotationAnim = getRotateAnimation()
+        circularSticksBaseView.startAnimation(rotationAnim)
+    }
+
+    private fun getRotateAnimation(): RotateAnimation {
+
+        val transAnim = RotateAnimation(0f, 360f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f)
+        transAnim.duration = animDuration.toLong()
+        transAnim.fillAfter = true
+        transAnim.repeatCount = Animation.INFINITE
+        transAnim.repeatMode = Animation.RESTART
+        transAnim.interpolator = LinearInterpolator()
+
+        return transAnim
+    }
+}
